f29e6a7bad0c825547f699396ddc611a14020c67
cesarsilva12344 <cesaronesto@gmail.com>
2025-12-09 18:27:49 -0300

    Add files via upload

--- js/secrets.js
+++ js/secrets.js
@@ -3,3 +3,4 @@ const SECRETS = {
     SUPABASE_URL: "https://seu-projeto.supabase.co",
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtcm1iZGR3bHdob2hqYnZreG1mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNjMzNDgsImV4cCI6MjA3OTgzOTM0OH0.Xk5hTWaiLKnJUfUv0ePFINUpwSBSg5qptVmswpMHdng"
 };
+    
